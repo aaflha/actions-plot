0bfbb6157355b06071399bba3cca5f23ac88397b
Kristine Flacké Haualand <78503551+krifla@users.noreply.github.com>
2025-05-07 21:28:50 +0200

    test new plot

--- main.py
+++ main.py
@@ -3,6 +3,52 @@ import numpy as np
 import datetime
 import pandas as pd
 import os
+import matplotlib.path as mpath
+
+
+def get_hull():
+    verts1 = np.array([[0,-128],[70,-128],[128,-70],[128,0],
+                      [128,32.5],[115.8,61.5],[96,84.6],[96,288],
+                      [96,341],[53,384],[0,384]])
+    verts2 = verts1[:-1,:] * np.array([-1,1])
+    codes1 = [1,4,4,4,4,4,4,2,4,4,4]
+    verts3 = np.array([[0,-80],[44,-80],[80,-44],[80,0],
+                      [80,34.3],[60.7,52],[48,66.5],[48,288],
+                      [48,314],[26.5,336],[0,336]])
+    verts4 = verts3[:-1,:] * np.array([-1,1])
+    verts = np.concatenate((verts1, verts2[::-1], verts4, verts3[::-1]))
+    codes = codes1 + codes1[::-1][:-1]
+    return mpath.Path(verts/256., codes+codes)
+
+def get_mercury(s=1):
+    a = 0; b = 64; c = 35
+    d = 320 - b
+    e = (1-s)*d
+    verts1 = np.array([[a,-b],[c,-b],[b,-c],[b,a],[b,c],[c,b],[a,b]])
+    verts2 = verts1[:-1,:] * np.array([-1,1])
+    verts3 = np.array([[0,0],[32,0],[32,288-e],[32,305-e],
+                       [17.5,320-e],[0,320-e]])
+    verts4 = verts3[:-1,:] * np.array([-1,1])
+    codes = [1] + [4]*12 + [1,2,2,4,4,4,4,4,4,2,2]
+    verts = np.concatenate((verts1, verts2[::-1], verts3, verts4[::-1]))
+    return mpath.Path(verts/256., codes)
+
+def scatter(self, x,y, temp=1, tempnorm=None, ax=None, **kwargs):
+    self.ax = ax or plt.gca()
+    temp = np.atleast_1d(temp)
+    ec = kwargs.pop("edgecolor", "black")
+    kwargs.update(linewidth=0)
+    self.inner = self.ax.scatter(x,y, **kwargs)
+    kwargs.update(c=None, facecolor=ec, edgecolor=None, color=None)
+    self.outer = self.ax.scatter(x,y, **kwargs)
+    self.outer.set_paths([self.get_hull()])
+    if not tempnorm:
+        mi, ma = np.nanmin(temp), np.nanmax(temp)
+        if mi == ma:
+            mi=0
+        tempnorm = plt.Normalize(mi,ma)
+    ipaths = [self.get_mercury(tempnorm(t)) for t in temp]
+    self.inner.set_paths(ipaths)
 
 def plot():
     NB = pd.read_csv(str(os.environ["HOURLY_SECRET"]), sep=',')
@@ -26,26 +72,25 @@ def plot():
 
     ### plot data
 
-    start = np.where(NB['date'] >= pd.Timestamp('2023-10-10 00:00:00'))[0][0]
-    end = -1#np.where(NB['date'] >= pd.Timestamp('2022-10-11 00:00:00'))[0][0]
-
-    c = [u'#1f77b4', u'#ff7f0e', u'#2ca02c', u'#d62728', u'#9467bd', u'#8c564b', u'#e377c2', u'#7f7f7f', u'#bcbd22', u'#17becf']
-
-    fig,(ax1) = plt.subplots(1,figsize=(20,8),dpi=50)
-    plt.rcParams.update({'font.size': 22})
-
-    ax1.plot(NB['date'],NB['p_u'],'-',c=c[0])
-    ax1.set_title('last datetime in csv-file: %s\ncsv-file last checked: %s' % (NB['date'].max(),datetime.datetime.now()))
-    #ax.plot(NB['date'],NB['p_u'],marker='.',c=c[0])
-    ax1b = ax1.twinx()
-    ax1b.plot(NB['date'],NB['mslp'],c=c[1])
-    ax1.set_ylabel('pressure (hPa)', c=c[0])
-    ax1b.set_ylabel('mean sea level pressure (hPa)', rotation=270, labelpad=25, c=c[1])
-    ax1.tick_params(axis='y', colors=c[0])
-    ax1b.tick_params(axis='y', colors=c[1])
-    ax1.set_xlim(xmin=NB['date'].values[start],xmax=NB['date'].values[end])
-    #plt.xticks(rotation=45)#, ha='right')
-    fig.autofmt_xdate(rotation=45)
+    fig, ax = plt.subplots(figsize=(10,5))
+    n = 42
+    x = NB['date'].values[-49:-1]
+    y = NB['t_u'].values[-49:-1]
+    ymin = round(np.min(y))-1
+    ymax = round(np.max(y))+3
+    sind = 3
+    ind = 6
+    
+    ax.plot(x,y, color="darkgrey", lw=2.5)
+    
+    p = TemperaturePlot()
+    p.scatter(x[sind::ind],y[sind::ind]+2, s=300, temp=y[sind::ind], c=y[sind::ind], edgecolor="k", cmap="RdYlBu_r")
+    
+    ax.set_ylim(ymin,ymax)
+    ax.set_xticks([NB['date'].values[-49],NB['date'].values[-37],NB['date'].values[-25],NB['date'].values[-13],NB['date'].values[-1]])
+    ax.set_xticklabels(['48','36','24', '12', '0'])
+    ax.set_xlabel('Hours ago')
+    ax.set_ylabel('Temperature (\u00b0C)')
 
     plt.savefig('fig.png')
 
